82988ddb32098a6ed486a7549bed41682a5391aa
Saeed pk <saeedpk781@gmail.com>
2024-10-20 15:29:10 +0530

    Some backend change

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -6,7 +6,7 @@ export async function POST(req:Request) {
     try {
         const formData = await req.json()
         const { adminCode , password } = formData;
-        if(adminCode === process.env.WEBCODGE_ADMIN_CODE || password === process.env.WEBCODGE_PASSWORD){
+        if(adminCode === process.env.WEBCODGE_ADMIN_CODE && password === process.env.WEBCODGE_PASSWORD){
             return NextResponse.json({message:"Form data received successfully!"})
         }else{
             return NextResponse.json({error:"Failed to process form data"},{status:500})
